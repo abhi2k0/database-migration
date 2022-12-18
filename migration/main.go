dbb5d74fbffae587ad60e7aea837ea6ab4dd86f6
abhi2k0 <abhishek.gupta2k0@gmail.com>
2022-12-18 22:30:11 +0530

    added db migrations

---
+++ migration/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	m, err := migrate.New(
+		"file://db/",
+		"postgres://postgres:postgres@localhost:5432/test?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+		log.Fatal(err)
+	}
+}
